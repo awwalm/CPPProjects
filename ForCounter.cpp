f9f783ddfc9b70e2514b47b4bae21b71cadf9505
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-05-29 09:37:44 +0400

    Added ForCounter.cpp

---
+++ ForCounter.cpp
@@ -0,0 +1,14 @@
+// Fig. 5.2: ForCounter.cpp
+// Counter-controlled iteration with the for iteration statement.
+#include <iostream>
+using namespace std;
+
+int main() {
+    // for statement header includes initialization,
+    // loop-continuation condition and increment
+    for (unsigned int counter{1}; counter <= 10; ++counter) {
+        cout << counter << " ";
+        }
+
+    cout << endl;
+}
